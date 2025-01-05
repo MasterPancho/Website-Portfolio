c6fc032412ce0008beef840c9c21d60b7aa04abc
Miguel <miguelvn13@gmail.com>
2025-01-04 20:43:15 -0500

    Sanitized user input.

--- email_backend/server.js
+++ email_backend/server.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer');                                       //Module to send emails from the server
 const express = require('express');                                             //Simplify process of building web servers (handle HTTP request, routes, middleware...)     
 const cors = require('cors');                                                   //Allows for cross-origin resource sharing (CORS) in the server (requests from different domains)
+const validator = require('validator');                                         //To validate and sanitize user inputs
 require('dotenv').config();                                                     //Load environment variables from a .env file into process.env
             
 const app = express();                  
-const PORT = process.env.PORT || 5000;                                          //Port the server will listen on. Else, it will default to 5000 (when it runs locally for instance)
-            
+const PORT = process.env.PORT                                                   //Port the server will listen on. Else, it will default to 5000 (when it runs locally for instance)
+
 app.use(cors({origin: 'https://masterpancho.github.io'}));                      //Allows for requests from Github Pages domain            
 app.use(express.json());                                                        //Middleware to parse incoming data raquests with JSON payloads (user info). Use req.body to handle this data
             
@@ -18,6 +19,14 @@ app.post('/send-email', async (req, res) => {                                   
     //Verify input data
     if(!name || !email || !message)
         return res.status(400).json({error: 'All fields are required.'});       //res.status(400) -> Sends a bad request status code
+           
+    if (!validator.isEmail(email))                                               //Checks if email is valid
+        return res.status(400).json({ error: 'Invalid email address.' });
+
+    // Input Sanitization
+    const sanitizedName = validator.escape(name);                             
+    const sanitizedEmail = validator.normalizeEmail(email);                             
+    const sanitizedMessage = validator.escape(message);
 
     try{
         //Configure how the email will be sent
@@ -28,24 +37,24 @@ app.post('/send-email', async (req, res) => {                                   
                 pass: process.env.GMAIL_PASS,                                 
             },
         });
-        
+
         //Configures email details
         const mailOptions = {
-            from: process.env.GMAIL_USER,                                     //Sender's email
-            to: process.env.GMAIL_USER,                                       //Recipient's email
-            subject: `New message from ${name} (${email})`,                   //Email subject
+            from: process.env.GMAIL_USER,                                           //Sender's email
+            to: process.env.GMAIL_USER,                                             //Recipient's email
+            subject: `New message from ${sanitizedName} (${sanitizedEmail})`,       //Email subject
             text: `You have a new contact form submission:
-                Name: ${name}
-                Email: ${email}
-                Message: ${message}`
+                Name: ${sanitizedName}
+                Email: ${sanitizedEmail}
+                Message: ${sanitizedMessage}`
         };
 
         // Send the email
         await transporter.sendMail(mailOptions);
         res.status(200).json({message: 'Email sent successfully!'});                //res.status(200) -> Sends a success status code
     }
-    catch(e){
-        console.error('Error sending email:', e);
+    catch{
+        console.error('Error sending email:');
         res.status(500).json({error: 'Failed to send email.'});                     //res.status(500) -> Sends an internal server error status code
     }
 });
