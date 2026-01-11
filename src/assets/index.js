5221ad4cb96e943ffbf66d1b611330dd99e7aafa
Miguel <miguelvn13@gmail.com>
2026-01-10 22:37:04 -0500

    Remove images from git tracking.

---
+++ src/assets/index.js
@@ -0,0 +1,99 @@
+import logo from "./logo.svg";
+import github from "./github.webp";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import language from "./medals/language.webp";
+import softdev from "./medals/softdev.webp";
+import gpa from "./medals/gpa.webp";
+import graduation from "./medals/graduation.webp";
+
+import css from "./tech/css.webp";
+import docker from "./tech/docker.webp";
+import git from "./tech/git.webp";
+import github_dark from "./tech/github.webp";
+import html from "./tech/html.webp";
+import javascript from "./tech/javascript.webp";
+import nodejs from "./tech/nodejs.webp";
+import reactjs from "./tech/reactjs.webp";
+import tailwind from "./tech/tailwind.webp";
+import threejs from "./tech/threejs.webp";
+import cpp from "./tech/cpp.webp";
+import c from "./tech/c.webp";
+import java from "./tech/java.webp";
+import kubernetes from "./tech/kubernetes.webp";
+import linux from "./tech/linux.webp";
+import office from "./tech/office.webp";
+import powershell from "./tech/powershell.webp";
+import python from "./tech/python.webp";
+import sql from "./tech/sql.webp";
+import ubuntu from "./tech/ubuntu.webp";
+import vite from "./tech/vite.webp";
+import vscode from "./tech/vscode.webp";
+import vstudio from "./tech/vstudio.webp";
+
+import jays from "./company/jays.webp";
+import hitachi from "./company/hitachi.webp";
+
+import atari from "./works/atari.webp";
+import bookstore from "./works/bookstore.webp";
+import weather from "./works/weather.webp";
+import dice from "./works/dice.webp";
+
+import ieee from "./achievements/certificates/ieee.png"
+import udemy from "./achievements/certificates/udemy.png"
+import hitachicoop from "./achievements/certificates/hitachicoop.png"
+
+import stadiumstar from "./achievements/awards/stadiumstar.png"
+import pokeith from "./achievements/awards/pokeith.png"
+import dean2022 from "./achievements/awards/dean2022.png"
+import dean2023 from "./achievements/awards/dean2023.png"
+import dean2024 from "./achievements/awards/dean2024.png"
+
+export {
+  logo,
+  github,
+  github_dark,
+  menu,
+  close,
+  language,
+  softdev,
+  gpa,
+  graduation,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  jays,
+  hitachi,
+  atari,
+  bookstore,
+  weather,
+  dice,
+  cpp,
+  c,
+  java ,
+  kubernetes,
+  linux,
+  office,
+  powershell,
+  python,
+  sql,
+  ubuntu,
+  vite,
+  vscode,
+  vstudio,
+  ieee,
+  udemy,
+  hitachicoop,
+  stadiumstar,
+  pokeith,
+  dean2022,
+  dean2023,
+  dean2024,
+};
